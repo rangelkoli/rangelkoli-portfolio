5fdbc81b15b2ec7e421f4042cf576300462feedb
Rangel Koli <77476841+rangelkoli@users.noreply.github.com>
2025-08-12 20:02:51 -0400

    refactor: remove dynamic import and mobile detection logic from Home component to simplify code

--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,10 @@ import AboutSection from "@/components/AboutSection";
 import HeroSection from "@/components/HeroSection";
 import FeaturedProjects from "@/components/FeaturedProjects";
 import Footer from "@/components/Footer";
-import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 import SkillsSection from "@/components/SkillsSection";
 
-const MobileSection = dynamic(() => import("@/components/MobileSection"), {
-  ssr: false,
-});
-
 export default function Home() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <main className='p-0 m-0 min-h-screen'>
       <HeroSection />
